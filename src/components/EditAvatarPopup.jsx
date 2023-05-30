116244ad3b634c59e907d997849b2ce4cee6eb97
sysoev-kirill <sysoev.kirill@gmail.com>
2023-05-31 00:05:46 +0300

    fix some bugs

--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -2,42 +2,46 @@ import { useEffect, useRef, useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 
-function EditAvatarPopup({isOpen, onClose, onUpdateAvatar}){
+function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
 
-const avatarInput = useRef();
+	const avatarInput = useRef();
 
 
-function handleSubmit(e){
-e.preventDefault();
-onUpdateAvatar({
-	avatar: avatarInput.current.value,
- });
-}
+	function handleSubmit(e) {
+		e.preventDefault();
+		onUpdateAvatar({
+			avatar: avatarInput.current.value,
+		});
+	}
 
-return (
-	<PopupWithForm
-	name='edit-avatar'
-	isOpen={isOpen}
-	onClose={onClose}
-	title={`Обновить аватар`}
-	buttonText={`Сохранить`}
-   onSubmit = {handleSubmit}
+	useEffect(() => {
+		avatarInput.current.value = " "
+	}, [isOpen])
 
- >
-	<input
-	  id="avatar__photo"
-	  placeholder="Ссылка на аватар"
-	  name="link"
-	  type="url"
-	  className="popup__profile popup__profile_add_photo"
-	  ref = {avatarInput}
-	  required
-	/>
-	<span className="popup__error" id="profile_photo_error"></span>
+	return (
+		<PopupWithForm
+			name='edit-avatar'
+			isOpen={isOpen}
+			onClose={onClose}
+			title={`Обновить аватар`}
+			buttonText={`Сохранить`}
+			onSubmit={handleSubmit}
 
- </PopupWithForm>
-	
-	
+		>
+			<input
+				id="avatar__photo"
+				placeholder="Ссылка на аватар"
+				name="link"
+				type="url"
+				className="popup__profile popup__profile_add_photo"
+				ref={avatarInput}
+				required
+			/>
+			<span className="popup__error" id="profile_photo_error"></span>
+
+		</PopupWithForm>
+
+
 	)
 
 
